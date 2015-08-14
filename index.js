ece214fd884e76427175b21f80fc127e6587aabb
Jordan Klassen <jordan@klassen.me.uk>
2015-08-14 00:33:47 -0700

    Implement line separator escaping

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var buffers = {
     escapeF: Buffer('\\f'),
     escapeN: Buffer('\\n'),
     escapeR: Buffer('\\r'),
-    escapeT: Buffer('\\t')
+    escapeT: Buffer('\\t'),
+    escapeLineSeparator: Buffer('\\u2028'),
+    escapeParagraphSeparator: Buffer('\\u2029')
 };
 
 for (var i = 0; i < 32; i++) {
@@ -25,6 +27,26 @@ var codes = {
     t: '\t'.charCodeAt(0)
 };
 
+var multiByte = {
+  'utf8': {
+      buffer: {
+          lineSeparator: Buffer('\u2028', 'utf8'),
+          paragraphSeparator: Buffer('\u2029', 'utf8')
+      },
+      codeToLength: {},
+      map: []
+  }
+}
+
+multiByte['utf8'].codeToLength[multiByte['utf8'].buffer.lineSeparator[0]] = multiByte['utf8'].buffer.lineSeparator.length;
+// first byte is the same in utf8, so paragraphSeparator isn't needed
+
+multiByte['utf8'].map.push({ k: multiByte['utf8'].buffer.lineSeparator, v: buffers.escapeLineSeparator });
+multiByte['utf8'].map.push({ k: multiByte['utf8'].buffer.paragraphSeparator, v: buffers.escapeParagraphSeparator });
+
+multiByte['buffer'] = multiByte['utf8'];
+
+
 var map = {};
 map[codes.quote] = buffers.escapeQuote;
 map[codes.escape] = buffers.escapeEscape;
@@ -41,6 +63,7 @@ module.exports = function () {
     
     function write (buf, enc, next) {
         var offset = 0;
+        var encoding = multiByte[enc || 'utf8'];
         for (var i = 0; i < buf.length; i++) {
             var c = buf[i];
             var m = map[c];
@@ -53,6 +76,22 @@ module.exports = function () {
                 var bufs = [ buf.slice(offset, i), buffers[c] ];
                 this.push(Buffer.concat(bufs));
                 offset = i + 1;
+            }
+            else if (encoding) {
+                var l = encoding.codeToLength[c];
+                if (l) {
+                    var codeBuf = buf.slice(i, i + l);
+                    for (var map_i = 0; map_i < encoding.map.length; map_i++) {
+                        var map_pair = encoding.map[map_i];
+                        if (codeBuf.equals(map_pair.k)) {
+                            var bufs = [ buf.slice(offset, i), map_pair.v ];
+                            this.push(Buffer.concat(bufs));
+                            offset = i + l;
+                            i += l - 1
+                            break;
+                        }
+                    }
+                }
             }
         }
         if (offset === 0) this.push(buf)
