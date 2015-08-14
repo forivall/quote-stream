96a23e8acbb8e5d08cb2bc266dd381896df65a6a
Jordan Klassen <jordan@klassen.me.uk>
2015-08-14 00:34:39 -0700

    Add utf16 tests

--- test/unicode_separators.js
+++ test/unicode_separators.js
@@ -21,3 +21,24 @@ test('utf8 separators', function (t) {
         t.equal(body.toString('utf8'), '"beep\\u2028ima\\u2029jeep"');
     }));
 });
+
+test('utf16le separators', function (t) {
+    t.plan(1);
+    var q = quote();
+    q.end(Buffer('beep\u2028ima\u2029jeep', 'utf16le'));
+
+    q.pipe(concat(function (body) {
+        console.log(body);
+        t.equal(body.toString('utf16le'), '"beep\\u2028ima\\u2029jeep"');
+    }));
+});
+
+test('ucs2 separators', function (t) {
+    t.plan(1);
+    var q = quote();
+    q.end(Buffer('beep\u2028ima\u2029jeep', 'ucs2'));
+
+    q.pipe(concat(function (body) {
+        t.equal(body.toString('ucs2'), '"beep\\u2028ima\\u2029jeep"');
+    }));
+});
