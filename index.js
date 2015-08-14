278f1d447c5d18e828f9aa98756e67bba81bdfb9
Jordan Klassen <jordan@klassen.me.uk>
2015-08-14 01:06:25 -0700

    wip utf16le support

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var through = require('through2');
-var buffers = {
-    quote: Buffer('"'),
-    escapeQuote: Buffer('\\"'),
-    escapeEscape: Buffer('\\\\'),
-    escapeB: Buffer('\\b'),
-    escapeF: Buffer('\\f'),
-    escapeN: Buffer('\\n'),
-    escapeR: Buffer('\\r'),
-    escapeT: Buffer('\\t'),
-    escapeLineSeparator: Buffer('\\u2028'),
-    escapeParagraphSeparator: Buffer('\\u2029')
+var strings = {
+    quote: '"',
+    escapeQuote: '\\"',
+    escapeEscape: '\\\\',
+    escapeB: '\\b',
+    escapeF: '\\f',
+    escapeN: '\\n',
+    escapeR: '\\r',
+    escapeT: '\\t',
+    escapeLineSeparator: '\\u2028',
+    escapeParagraphSeparator: '\\u2029'
 };
 
 for (var i = 0; i < 32; i++) {
     var s = i.toString(16);
-    buffers[i] = Buffer('\\u' + Array(5-s.length).join('0') + s);
+    strings[i] = '\\u' + Array(5-s.length).join('0') + s;
 }
 
 var codes = {
@@ -27,55 +27,90 @@ var codes = {
     t: '\t'.charCodeAt(0)
 };
 
-var multiByte = {
+var encodings = {
   'utf8': {
-      buffer: {
+      increment: 1,
+      buffers: {},
+      multiBytes: {
           lineSeparator: Buffer('\u2028', 'utf8'),
           paragraphSeparator: Buffer('\u2029', 'utf8')
       },
       codeToLength: {},
-      map: []
+      multiByteMap: [],
+      map: {}
+  },
+  'utf16le': {
+      increment: 2,
+      buffers: {},
+      multiBytes: {
+          lineSeparator: Buffer('\u2028', 'utf16le'),
+          paragraphSeparator: Buffer('\u2029', 'utf16le')
+      },
+      codeToLength: {},
+      multiByteMap: [],
+      map: {}
   }
 }
+for (var k in strings) {
+  encodings['utf8'].buffers[k] = Buffer(strings[k]);
+  encodings['utf16le'].buffers[k] = Buffer(strings[k], 'utf16le');
+}
 
-multiByte['utf8'].codeToLength[multiByte['utf8'].buffer.lineSeparator[0]] = multiByte['utf8'].buffer.lineSeparator.length;
+encodings['utf8'].codeToLength[encodings['utf8'].multiBytes.lineSeparator[0]] = encodings['utf8'].multiBytes.lineSeparator.length;
 // first byte is the same in utf8, so paragraphSeparator isn't needed
+encodings['utf16le'].codeToLength[encodings['utf16le'].multiBytes.lineSeparator[0]] = encodings['utf16le'].multiBytes.lineSeparator.length;
+encodings['utf16le'].codeToLength[encodings['utf16le'].multiBytes.paragraphSeparator[0]] = encodings['utf16le'].multiBytes.paragraphSeparator.length;
 
-multiByte['utf8'].map.push({ k: multiByte['utf8'].buffer.lineSeparator, v: buffers.escapeLineSeparator });
-multiByte['utf8'].map.push({ k: multiByte['utf8'].buffer.paragraphSeparator, v: buffers.escapeParagraphSeparator });
+encodings['utf8'].multiByteMap.push({ k: encodings['utf8'].multiBytes.lineSeparator, v: Buffer(strings.escapeLineSeparator, 'utf8') });
+encodings['utf8'].multiByteMap.push({ k: encodings['utf8'].multiBytes.paragraphSeparator, v: Buffer(strings.escapeParagraphSeparator, 'utf8') });
+encodings['utf16le'].multiByteMap.push({ k: encodings['utf16le'].multiBytes.lineSeparator, v: Buffer(strings.escapeLineSeparator, 'utf16le') });
+encodings['utf16le'].multiByteMap.push({ k: encodings['utf16le'].multiBytes.paragraphSeparator, v: Buffer(strings.escapeParagraphSeparator, 'utf16le') });
 
-multiByte['buffer'] = multiByte['utf8'];
+encodings['utf8'].map[codes.quote] = encodings['utf8'].buffers.escapeQuote;
+encodings['utf8'].map[codes.escape] = encodings['utf8'].buffers.escapeEscape;
+encodings['utf8'].map[codes.b] = encodings['utf8'].buffers.escapeB;
+encodings['utf8'].map[codes.f] = encodings['utf8'].buffers.escapeF;
+encodings['utf8'].map[codes.n] = encodings['utf8'].buffers.escapeN;
+encodings['utf8'].map[codes.r] = encodings['utf8'].buffers.escapeR;
+encodings['utf8'].map[codes.t] = encodings['utf8'].buffers.escapeT;
 
+encodings['utf16le'].map[codes.quote] = encodings['utf8'].buffers.escapeQuote;
+encodings['utf16le'].map[codes.escape] = encodings['utf8'].buffers.escapeEscape;
+encodings['utf16le'].map[codes.b] = encodings['utf8'].buffers.escapeB;
+encodings['utf16le'].map[codes.f] = encodings['utf8'].buffers.escapeF;
+encodings['utf16le'].map[codes.n] = encodings['utf8'].buffers.escapeN;
+encodings['utf16le'].map[codes.r] = encodings['utf8'].buffers.escapeR;
+encodings['utf16le'].map[codes.t] = encodings['utf8'].buffers.escapeT;
 
-var map = {};
-map[codes.quote] = buffers.escapeQuote;
-map[codes.escape] = buffers.escapeEscape;
-map[codes.b] = buffers.escapeB;
-map[codes.f] = buffers.escapeF;
-map[codes.n] = buffers.escapeN;
-map[codes.r] = buffers.escapeR;
-map[codes.t] = buffers.escapeT;
+encodings['ucs2'] = encodings['utf16le'];
+encodings['default'] = encodings['utf8'];
 
 module.exports = function () {
     var stream = through(write, end);
-    stream.push(buffers.quote);
+    var firstEncoding;
     return stream;
-    
+
     function write (buf, enc, next) {
+        if (!firstEncoding) {
+          firstEncoding = enc || 'utf8';
+          stream.push((encodings[firstEncoding] || encodings['default']).buffers.quote);
+        }
         var offset = 0;
-        var encoding = multiByte[enc || 'utf8'];
-        for (var i = 0; i < buf.length; i++) {
+        var encoding = encodings[enc || 'utf8'] || encodings['default'];
+        var inc = encoding.increment;
+        for (var i = 0; i < buf.length; i += inc) {
             var c = buf[i];
-            var m = map[c];
+            var m = encoding.map[c];
+
             if (m) {
-                var bufs = [ buf.slice(offset, i), m ];
+                var bufs = [ buf.slice(offset, i + inc - 1), m ];
                 this.push(Buffer.concat(bufs));
-                offset = i + 1;
+                offset = i + inc;
             }
             else if (c < 32) {
-                var bufs = [ buf.slice(offset, i), buffers[c] ];
+                var bufs = [ buf.slice(offset, i + inc - 1), encoding.buffers[c] ];
                 this.push(Buffer.concat(bufs));
-                offset = i + 1;
+                offset = i + inc;
             }
             else if (encoding) {
                 var l = encoding.codeToLength[c];
@@ -87,7 +122,7 @@ module.exports = function () {
                             var bufs = [ buf.slice(offset, i), map_pair.v ];
                             this.push(Buffer.concat(bufs));
                             offset = i + l;
-                            i += l - 1
+                            i += l - inc
                             break;
                         }
                     }
@@ -99,7 +134,11 @@ module.exports = function () {
         next();
     }
     function end (next) {
-        this.push(buffers.quote);
+        if (!firstEncoding) {
+          firstEncoding = enc || 'utf8';
+          stream.push((encodings[firstEncoding] || encodings['default']).buffers.quote);
+        }
+        stream.push((encodings[firstEncoding] || encodings['default']).buffers.quote);
         this.push(null);
     }
 };
